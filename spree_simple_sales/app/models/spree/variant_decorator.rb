f0507182e85cf57a6ebfc601bc659dc342f5f939
Naohiro OHTA <naohta@gmail.com>
2014-01-16 17:20:40 +0900

    wip

--- spree_simple_sales/app/models/spree/variant_decorator.rb
+++ spree_simple_sales/app/models/spree/variant_decorator.rb
@@ -1,7 +1,11 @@
+#coding:utf-8
 module Spree
   Variant.class_eval do
     alias_method :orig_price_in, :price_in
     def price_in(currency)
+      p "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+      p "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+      p "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
       return orig_price_in(currency) unless sale_price.present?
       Spree::Price.new(:variant_id => self.id, :amount => self.sale_price, :currency => currency)
     end
